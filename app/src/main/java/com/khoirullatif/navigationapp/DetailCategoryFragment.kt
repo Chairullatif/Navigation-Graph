441d133d136b77a7720596e8dbc968e13d8ef7e0
Chairullatif Aji Sadewa <khoirullatif337@gmail.com>
2021-07-17 13:16:15 +0700

    Menambahkan pop up behaviour

--- app/src/main/java/com/khoirullatif/navigationapp/DetailCategoryFragment.kt
+++ app/src/main/java/com/khoirullatif/navigationapp/DetailCategoryFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.findNavController
 import com.khoirullatif.navigationapp.databinding.FragmentDetailCategoryBinding
 
 class DetailCategoryFragment : Fragment() {
@@ -34,6 +35,10 @@ class DetailCategoryFragment : Fragment() {
 
         binding.tvCategoryName.text = dataName
         binding.tvCategoryDescription.text = "Level: $dataDesc"
+
+        binding.btnProfile.setOnClickListener {
+            it.findNavController().navigate(R.id.action_detailCategoryFragment_to_homeFragment)
+        }
     }
 
     override fun onDestroy() {
